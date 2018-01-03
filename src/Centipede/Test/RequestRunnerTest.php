0e4389c719eec9f25639e380d2a18a1986e7b8b6
Thibaut Vandenbussche <thibaut.vandenbussche@showpad.com>
2018-01-03 18:38:15 +0100

    Add basic request runner tests

---
+++ src/Centipede/Test/RequestRunnerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Centipede\Test;
+
+use Exception;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class RequestRunnerTest
+ */
+class RequestRunnerTest extends AbstractTestCase
+{
+    /**
+     * @return void
+     */
+    public function testSingleRequest() : void
+    {
+        $this->client->run(
+            function (ResponseInterface $response) {
+                $this->assertEquals(200, $response->getStatusCode());
+                $this->assertTrue(!!preg_match('/<title>Google<\/title>/m', $response->getBody()->getContents()));
+            },
+            function (Exception $exception) {
+                $this->fail($exception->getMessage());
+            },
+            new Request('GET', 'https://www.google.com')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testDisallowRedirects() : void
+    {
+        $this->client->setOptions([
+            'allow_redirects' => false,
+        ]);
+
+        $this->client->run(
+            function (ResponseInterface $response) {
+                $this->assertEquals(301, $response->getStatusCode());
+            },
+            function (Exception $exception) {
+                $this->fail($exception->getMessage());
+            },
+            new Request('GET', 'http://thibaut.sh')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testAllowRedirects() : void
+    {
+        $this->client->setOptions([
+            'allow_redirects' => true,
+        ]);
+
+        $this->client->run(
+            function (ResponseInterface $response) {
+                $this->assertEquals(200, $response->getStatusCode());
+            },
+            function (Exception $exception) {
+                $this->fail($exception->getMessage());
+            },
+            new Request('GET', 'http://thibaut.sh')
+        );
+    }
+}
